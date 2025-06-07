9c7eadddaac4b4d1a8b8ac81f724917e0d131f58
YourmixJNR <radmakson@gmail.com>
2025-06-07 12:45:07 +0100

    Refactor ERC20 test setup; improve readability and consistency in token minting and transfer assertions

--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -1,26 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2, StdStyle} from "forge-std/Test.sol";
 import {ERC20} from "../src/ERC20.sol";
 
-contract ERC20Test is ERC20, Test {
+contract BsseSetup is ERC20, Test {
     // test addresses
-    address private alice;
-    address private bob;
+    address internal alice;
+    address internal bob;
 
     // init ERC20 with name, symbol, and decimals
-    constructor() ERC20("name", "SYM", 19) {}
+    constructor() ERC20("name", "SYM", 18) {}
 
     // Setup function runs before each test
-    function setUp() public {
+    function setUp() public virtual {
         // create a test address for Alice
         alice = makeAddr("alice");
         // create a test address for Bob
         bob = makeAddr("bob");
 
-        // mint 200 tokens to Alice
-        _mint(alice, 200);
+        console2.log(StdStyle.blue("When Alice has 200 tokens"));
+        _mint(alice, 200e18);
+    }
+}
+
+contract ERC20TransferSetup is BsseSetup {
+    function setUp() public override {
+        BsseSetup.setUp();
     }
 
     // test transferring tokens from Alice to Bob
@@ -29,9 +35,15 @@ contract ERC20Test is ERC20, Test {
         vm.prank(alice);
 
         // transfer 100 tokens to Bob
-        bool success = this.transfer(bob, 100);
+        bool success = this.transfer(bob, 100e18);
 
         // assert that the transfer was successful
         assertTrue(success);
+
+        // assert that Alice's balance is now 100 tokens
+        assertEqDecimal(this.balanceOf(alice), 100e18, decimal);
+
+        // assert that Bob's balance is now 100 tokens
+        assertEqDecimal(this.balanceOf(bob), 100e18, decimal);
     }
 }
